aa5530bb91de311a3156227cf7a6dba4fbe733b5
maxwen <max.weninger@gmail.com>
2013-12-06 12:45:36 +0100

    [1/2] base: added config for menu unlock
    Allow to define if menu unlock is enabled or not
    
    Change-Id: Ibe498f5b6fc956678f426c9fdc9db28a0c483429

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3007,6 +3007,12 @@ public final class Settings {
         public static final String STATUS_BAR_QS_QUICK_PULLDOWN = "status_bar_qs_quick_pulldown";
 
         /**
+         * Whether to unlock the menu key.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String MENU_UNLOCK_SCREEN = "menu_unlock_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
