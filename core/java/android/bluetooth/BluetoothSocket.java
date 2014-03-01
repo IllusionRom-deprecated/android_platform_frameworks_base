b8bd3d595236625b2ed0469b1790411a89e556fc
pramod kotreshappa <pramodk@codeaurora.org>
2014-02-28 17:29:31 -0800

    Bluetooth: Fix for FileDescriptor leakage causing ANR
    Fix to close the native fds gracefully. detachFD has been removed
    in order to close the native fds
    
    Change-Id: I6afca60bcf6adc82ad6a63d8706267d8ed3bd579
    CRs-fixed: 623575

--- core/java/android/bluetooth/BluetoothSocket.java
+++ core/java/android/bluetooth/BluetoothSocket.java
@@ -518,7 +518,6 @@ public final class BluetoothSocket implements Closeable {
                     mSocket = null;
                 }
                 if(mPfd != null) {
-                    mPfd.detachFd();
                     mPfd.close();
                     mPfd = null;
                 }
