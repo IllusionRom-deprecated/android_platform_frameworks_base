CyanogenMod/android_frameworks_base

Base: On-The-Go: Allow to restart service on the fly
add a toggle to the options dialog to allow the user to restart the service
on the fly, instead of stopping it and create a notification.

it maybe be buggy, depending on how quick their camera can release and
recreate the instance, thats why i went the stop the service way by default

if the user chooses to recreate it on the fly, it resets the views,
triggers a handler with a delay of 1/2 second and sets up the views again

also increate timeout of the dialog, as the transparent dialog may be
harder to read for users and its annoying if the dialog disappears while
you search for the setting you want.

Change-Id: I1233854c10ee524925834c73ea20fba44c0f2a46

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoDialog.java
@@ -53,9 +53,9 @@ public class OnTheGoDialog extends Dialog {
         mContext = ctx;
         final Resources r = mContext.getResources();
         mOnTheGoDialogLongTimeout =
-                r.getInteger(R.integer.quick_settings_brightness_dialog_long_timeout);
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
         mOnTheGoDialogShortTimeout =
-                r.getInteger(R.integer.quick_settings_brightness_dialog_short_timeout);
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
     }
 
     @Override
@@ -96,6 +96,21 @@ public class OnTheGoDialog extends Dialog {
             }
         });
 
+        final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+        final boolean restartService = Settings.Nameless.getBoolean(resolver,
+                Settings.Nameless.ON_THE_GO_SERVICE_RESTART,
+                false);
+        mServiceToggle.setChecked(restartService);
+        mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Settings.Nameless.putBoolean(resolver,
+                        Settings.Nameless.ON_THE_GO_SERVICE_RESTART,
+                        b);
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
         final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
         final boolean useFrontCam = (Settings.Nameless.getInt(resolver,
                 Settings.Nameless.ON_THE_GO_CAMERA,
@@ -107,7 +122,7 @@ public class OnTheGoDialog extends Dialog {
                 Settings.Nameless.putInt(resolver,
                         Settings.Nameless.ON_THE_GO_CAMERA,
                         (b ? 1 : 0));
-                mHandler.post(mDismissDialogRunnable);
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
             }
         });
     }

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java
@@ -115,8 +115,15 @@ public class OnTheGoService extends Service {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
-            // Stop and restart
-            stopOnTheGo(true);
+            final ContentResolver resolver = getContentResolver();
+            final boolean restartService = Settings.Nameless.getBoolean(resolver,
+                    Settings.Nameless.ON_THE_GO_SERVICE_RESTART,
+                    false);
+            if (restartService) {
+                restartOnTheGo();
+            } else {
+                stopOnTheGo(true);
+            }
         }
     }
 
@@ -147,7 +154,7 @@ public class OnTheGoService extends Service {
 
         resetViews();
         registerAlphaReceiver();
-        setupViews();
+        setupViews(false);
         final SettingsObserver mObserver = new SettingsObserver(mHandler);
         mObserver.observe();
 
@@ -170,6 +177,19 @@ public class OnTheGoService extends Service {
 
         stopSelf();
     }
+
+    private void restartOnTheGo() {
+        resetViews();
+        mHandler.removeCallbacks(mRestartRunnable);
+        mHandler.postDelayed(mRestartRunnable, 500);
+    }
+
+    private final Runnable mRestartRunnable = new Runnable() {
+        @Override
+        public void run() {
+            setupViews(true);
+        }
+    };
 
     private void toggleOnTheGoAlpha() {
         final float alpha = Settings.Nameless.getFloat(getContentResolver(),
@@ -216,7 +236,7 @@ public class OnTheGoService extends Service {
         }
     }
 
-    private void setupViews() {
+    private void setupViews(boolean isRestarting) {
         final int cameraType = Settings.Nameless.getInt(getContentResolver(),
                 Settings.Nameless.ON_THE_GO_CAMERA,
                 0);
@@ -224,8 +244,10 @@ public class OnTheGoService extends Service {
             getCameraInstance(cameraType);
         } catch (RuntimeException exc) {
             // Well, you cant have all in this life..
-            createNotification(NOTIFICATION_ERROR);
-            stopOnTheGo(true);
+            if (!isRestarting) {
+                createNotification(NOTIFICATION_ERROR);
+                stopOnTheGo(true);
+            }
         }
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(

IllusionRom-deprecated/android_platform_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoDialog.java
@@ -53,9 +53,9 @@ public class OnTheGoDialog extends Dialog {
         mContext = ctx;
         final Resources r = mContext.getResources();
         mOnTheGoDialogLongTimeout =
-                r.getInteger(R.integer.quick_settings_brightness_dialog_long_timeout);
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
         mOnTheGoDialogShortTimeout =
-                r.getInteger(R.integer.quick_settings_brightness_dialog_short_timeout);
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
     }
 
     @Override
@@ -95,6 +95,21 @@ public class OnTheGoDialog extends Dialog {
             }
         });
 
+        final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+        final boolean restartService = Settings.System.getBoolean(resolver,
+                Settings.System.ON_THE_GO_SERVICE_RESTART,
+                false);
+        mServiceToggle.setChecked(restartService);
+        mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Settings.System.putBoolean(resolver,
+                        Settings.System.ON_THE_GO_SERVICE_RESTART,
+                        b);
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
         final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
         final boolean useFrontCam = (Settings.System.getInt(resolver,
                 Settings.System.ON_THE_GO_CAMERA,
@@ -106,7 +121,7 @@ public class OnTheGoDialog extends Dialog {
                 Settings.System.putInt(resolver,
                         Settings.System.ON_THE_GO_CAMERA,
                         (b ? 1 : 0));
-                mHandler.post(mDismissDialogRunnable);
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
             }
         });
     }

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java
@@ -115,8 +115,15 @@ public class OnTheGoService extends Service {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
-            // Stop and restart
-            stopOnTheGo(true);
+            final ContentResolver resolver = getContentResolver();
+            final boolean restartService = Settings.System.getBoolean(resolver,
+                    Settings.System.ON_THE_GO_SERVICE_RESTART,
+                    false);
+            if (restartService) {
+                restartOnTheGo();
+            } else {
+                stopOnTheGo(true);
+            }
         }
     }
 
@@ -147,7 +154,7 @@ public class OnTheGoService extends Service {
 
         resetViews();
         registerAlphaReceiver();
-        setupViews();
+        setupViews(false);
         final SettingsObserver mObserver = new SettingsObserver(mHandler);
         mObserver.observe();
 
@@ -170,6 +177,19 @@ public class OnTheGoService extends Service {
 
         stopSelf();
     }
+
+    private void restartOnTheGo() {
+        resetViews();
+        mHandler.removeCallbacks(mRestartRunnable);
+        mHandler.postDelayed(mRestartRunnable, 500);
+    }
+
+    private final Runnable mRestartRunnable = new Runnable() {
+        @Override
+        public void run() {
+            setupViews(true);
+        }
+    };
 
     private void toggleOnTheGoAlpha() {
         final float alpha = Settings.System.getFloat(getContentResolver(),
@@ -216,7 +236,7 @@ public class OnTheGoService extends Service {
         }
     }
 
-    private void setupViews() {
+    private void setupViews(boolean isRestarting) {
         final int cameraType = Settings.System.getInt(getContentResolver(),
                 Settings.System.ON_THE_GO_CAMERA,
                 0);
@@ -224,8 +244,10 @@ public class OnTheGoService extends Service {
             getCameraInstance(cameraType);
         } catch (RuntimeException exc) {
             // Well, you cant have all in this life..
-            createNotification(NOTIFICATION_ERROR);
-            stopOnTheGo(true);
+            if (!isRestarting) {
+                createNotification(NOTIFICATION_ERROR);
+                stopOnTheGo(true);
+            }
         }
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
@@ -313,7 +335,7 @@ public class OnTheGoService extends Service {
 
         if (type == 1 || type == 2) {
             final ComponentName cn = new ComponentName("com.android.systemui",
-                    "com.android.systemui.nameless.onthego.OnTheGoService");
+                    "com.android.systemui.system.onthego.OnTheGoService");
             final Intent startIntent = new Intent();
             startIntent.setComponent(cn);
             startIntent.setAction(ACTION_START);