666dd1364e76641efa24ea21d1595aa814dfed79
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2014-04-03 21:05:12 +0000

    Frameworks: SlimRoms recents app
    Squash of new slimroms recent apps mode.
    It does not include TRDS since we will not support it.
    Also themized with Illusion drawables.
    
    All credits goes to Kufikugel and SlimRom team.
    
    Change-Id: I892b73f5c08a569fe2026be2c55696eeaaddb30b

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10893,4 +10893,10 @@ public class WindowManagerService extends IWindowManager.Stub
     public void addSystemUIVisibilityFlag(int flag) {
         mLastStatusBarVisibility |= flag;
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return mLastStatusBarVisibility;
+    }
 }
