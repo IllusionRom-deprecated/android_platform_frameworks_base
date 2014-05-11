32d679b10a8d6b587d4fa73aaa552cfebfdb94a9
AdityaSureka <adityasureka92@gmail.com>
2014-05-11 10:45:33 -0400

    Revert "[2/2] Framework/base: Lockscreen maximized widgets"
    This reverts commit 29d3b88aca9b4ddac03c7762c768ffd43cbb9b20.
    
    Change-Id: Id083750fd1091f2b939a3641fce4a74bbfa2dcf6

--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -405,7 +405,6 @@ public class KeyguardHostView extends KeyguardViewBase {
         showPrimarySecurityScreen(false);
         updateSecurityViews();
         enableUserSelectorIfNecessary();
-        minimizeChallengeIfDesired();
     }
 
     private void updateAndAddWidgets() {
@@ -1051,7 +1050,6 @@ public class KeyguardHostView extends KeyguardViewBase {
         }
 
         requestFocus();
-        minimizeChallengeIfDesired();
     }
 
     @Override
@@ -1105,19 +1103,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             // otherwise, go to the unlock screen, see if they can verify it
             mIsVerifyUnlockOnly = true;
             showSecurityScreen(securityMode);
-        }
-    }
-
-    private void minimizeChallengeIfDesired() {
-        if (mSlidingChallengeLayout == null) {
-            return;
-        }
-
-        int setting = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS, 0, UserHandle.USER_CURRENT);
-
-        if (setting == 1) {
-            mSlidingChallengeLayout.showChallenge(false);
         }
     }
 
