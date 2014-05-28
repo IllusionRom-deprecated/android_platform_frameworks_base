d78b706b61300a5751ffcdd01a8922c6d5dd5d4f
Aditya Sureka <adityasureka92@gmail.com>
2014-05-28 08:43:37 +0200

    Revert "Fix for default USB debugging in eng builds.  Starts adbd on first launch and accepts all connections for eng, starts adbd on first launch and prompts for userdebug, does not start adbd for user."
    This reverts commit df662965b33cac772ed5457e726067f49606d3c5.
    
    Change-Id: I0d79ab490ba39a806c065ad945c8dea5904d6229

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -30,7 +30,6 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbManager;
-import android.os.Bundle;
 import android.os.FileUtils;
 import android.os.Handler;
 import android.os.Looper;
@@ -125,15 +124,8 @@ public class UsbDeviceManager {
         }
         @Override
         public void onChange(boolean selfChange) {
-            String buildType = System.getProperty("ro.build.type");
-            int defInt;
-            if (buildType.equalsIgnoreCase("eng")) {
-                defInt = 1;
-            } else {
-                defInt = 0;
-            }
             boolean enable = (Settings.Global.getInt(mContentResolver,
-                    Settings.Global.ADB_ENABLED, defInt) > 0);
+                    Settings.Global.ADB_ENABLED, 0) > 0);
             mHandler.sendMessage(MSG_ENABLE_ADB, enable);
         }
     }
